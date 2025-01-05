bbe318e8507e95dfb5b78a2bfd50fcc531c0a41e
lnarolski <narolskilukasz+github@gmail.com>
2025-01-05 21:26:48 +0100

    Day2

--- AdventOfCode2024/Day2.cs
+++ AdventOfCode2024/Day2.cs
@@ -3,26 +3,132 @@ class Day2 : IAlgorithms, IStars
 {
     public Day2() {
         Star1();
-        //Star2();
-    }
-
-    public void Star1()
-    {
-        throw new NotImplementedException();
+        Star2();
     }
 
     public void Star1Algorithm(string filePath)
     {
-        throw new NotImplementedException();
-    }
+        var reports = IAlgorithms.LoadMatrix(filePath);
 
-    public void Star2()
-    {
-        throw new NotImplementedException();
+        long safeReports = 0;
+        foreach (var report in reports) {
+            bool safe = true;
+            bool ascending = false;
+            for (int i = 1; safe && i < report.Count; ++i) {
+                if (report[i] == report[i - 1]) {
+                    safe = false;
+                    break;
+                }
+
+                if (i == 1) {
+                    ascending = report[1] > report[0];
+                } else {
+                    if (report[i] > report[i - 1] && !ascending ||
+                        report[i] < report[i - 1] && ascending) {
+                        safe = false;
+                        break;
+                    }
+                }
+
+                if (Math.Abs(report[i] - report[i - 1]) > 3) {
+                        safe = false;
+                        break;
+                }
+            }
+
+            if (safe) {
+                safeReports += 1;
+            }
+        }
+
+        Console.WriteLine($"Star 1: {safeReports}");
     }
 
     public void Star2Algorithm(string filePath)
     {
-        throw new NotImplementedException();
+        var reports = IAlgorithms.LoadMatrix(filePath);
+
+        long safeReports = 0;
+        foreach (var report in reports) {
+            bool safe = true;
+            bool ascending = false;
+            bool warning = false;
+            for (int i = 0; i < report.Count - 1; ++i) {
+                if (report[i] == report[i + 1]) {
+                    safe = false;
+                    warning = true;
+                    break;
+                }
+
+                if (i == 0) {
+                    ascending = report[1] > report[0];
+                } else {
+                    if (report[i] > report[i + 1] && ascending ||
+                        report[i] < report[i + 1] && !ascending) {
+                        safe = false;
+                        warning = true;
+                        break;
+                    }
+                }
+
+                if (Math.Abs(report[i] - report[i + 1]) > 3) {
+                    safe = false;
+                    warning = true;
+                    break;
+                }
+            }
+
+            for (int indexToRemove = 0; warning && indexToRemove < report.Count; ++indexToRemove)
+            {
+                bool tempSafe = true;
+                var tempReport = new List<long>(report);
+                tempReport.RemoveAt(indexToRemove);
+
+                ascending = false;
+                for (int i = 0; i < tempReport.Count - 1; ++i) {
+                    if (tempReport[i] == tempReport[i + 1]) {
+                        tempSafe = false;
+                        break;
+                    }
+
+                    if (i == 0) {
+                        ascending = tempReport[1] > tempReport[0];
+                    } else {
+                        if (tempReport[i] > tempReport[i + 1] && ascending ||
+                            tempReport[i] < tempReport[i + 1] && !ascending) {
+                            tempSafe = false;
+                            break;
+                        }
+                    }
+
+                    if (Math.Abs(tempReport[i] - tempReport[i + 1]) > 3) {
+                        tempSafe = false;
+                        break;
+                    }
+                }
+
+                if (tempSafe) {
+                    safe = true;
+                }
+            }
+
+            if (safe) {
+                safeReports += 1;
+            }
+        }
+
+        Console.WriteLine($"Star 2: {safeReports}");
+    }
+
+    public void Star1() {
+        // Star1Algorithm("../../../Examples/Day2Star1Example1.txt");
+
+        Star1Algorithm("../../../Input/Day2Star1.txt");
+    }
+
+    public void Star2() {
+        // Star2Algorithm("../../../Examples/Day2Star1Example1.txt");
+
+        Star2Algorithm("../../../Input/Day2Star1.txt");
     }
 }
--- AdventOfCode2024/IAlgorithms.cs
+++ AdventOfCode2024/IAlgorithms.cs
@@ -2,4 +2,23 @@ public interface IAlgorithms
 {
     public void Star1Algorithm(string filePath);
     public void Star2Algorithm(string filePath);
+
+    public static List<List<long>> LoadMatrix(string filePath) {
+        var matrix = new List<List<long>>();
+
+        var file = File.OpenText(filePath);
+        var line = file.ReadLine();
+        while (line != null)
+        {
+            var columns = new List<long>();
+            foreach (var level in line.Split(' ')) {
+                columns.Add(Convert.ToInt64(level));
+            }
+            matrix.Add(columns);
+            
+            line = file.ReadLine();
+        }
+
+        return matrix;
+    }
 }
