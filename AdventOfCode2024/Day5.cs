1d063c53320fca9238d718f8d9bc77ab40f92981
lnarolski <narolskilukasz+github@gmail.com>
2025-01-10 18:52:55 +0100

    Day5

--- AdventOfCode2024/Day5.cs
+++ AdventOfCode2024/Day5.cs
@@ -10,7 +10,62 @@ class Day5 : IAlgorithms, IStars
     {
         long result = 0;
 
-        
+        var input = IAlgorithms.LoadLines(filePath);
+
+        var rules = new Dictionary<long, List<long>>();
+        var pageNumbers = new List<List<long>>();
+
+        bool parsingRules = true;
+        foreach (var item in input)
+        {
+            if (item.Equals("")) {
+                parsingRules = false;
+            } else {
+                if (parsingRules) {
+                    var temp = item.Split('|');
+                    var key = Convert.ToInt64(temp[0]);
+                    var value = Convert.ToInt64(temp[1]);
+
+                    if (!rules.ContainsKey(key)) {
+                        rules.Add(key, new List<long>());
+                    }
+                    rules[key].Add(value);
+                } else {
+                    var temp = item.Split(',');
+                    var convertedPageNumbers = new List<long>();
+                    foreach (var pageNumber in temp)
+                    {
+                        convertedPageNumbers.Add(Convert.ToInt64(pageNumber));
+                    }
+                    pageNumbers.Add(convertedPageNumbers);
+                }
+            }
+        }
+
+        foreach (var item in pageNumbers)
+        {
+            HashSet<long> pageNumbersSet = new HashSet<long>() {item[0]};
+            bool correctOrder = true;
+
+            for (int i = 1; correctOrder && i < item.Count; ++i) {
+                if (rules.ContainsKey(item[i])) {
+                    foreach (var ruleNumber in rules[item[i]]) {
+                        if (pageNumbersSet.Contains(ruleNumber)) {
+                            correctOrder = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (correctOrder) {
+                    pageNumbersSet.Add(item[i]);
+                }
+            }
+
+            if (correctOrder) {
+                result += item[item.Count / 2];
+            }
+        }
 
         Console.WriteLine($"Star 1: {result}");
     }
