abe6c9b3f8aa503aea5d365332b487f27757ad41
lnarolski <narolskilukasz+github@gmail.com>
2025-01-15 22:00:47 +0100

    Day9

--- AdventOfCode2024/Day9.cs
+++ AdventOfCode2024/Day9.cs
@@ -80,7 +80,71 @@ class Day9 : IAlgorithms, IStars
     {
         long result = 0;
 
-        
+        var input = IAlgorithms.LoadLines(filePath);
+        LinkedList<List<int>> inputList = new LinkedList<List<int>>();
+
+        int id = 0;
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            if (i % 2 == 0) {
+                inputList.AddLast(new List<int>{id, input[0][i] - '0'});
+                id++;
+            } else {
+                inputList.AddLast(new List<int>{-1, input[0][i] - '0'});
+            }
+        }
+
+        {
+        var backward = inputList.Last;
+        var forward = inputList.First;
+        while (inputList.First != backward) {
+            if (forward == backward) {
+                forward = inputList.First;
+                backward = backward.Previous;
+                continue;
+            }
+
+            if (forward.Value[0] != -1) {
+                forward = forward.Next;
+                continue;
+            }
+
+            if (backward.Value[0] == -1) {
+                backward = backward.Previous;
+                continue;
+            }
+
+            if (backward.Value[1] == forward.Value[1]) {
+                forward.Value[0] = backward.Value[0];
+                backward.Value[0] = -1;
+
+                forward = inputList.First;
+                backward = backward.Previous;
+                continue;
+            } else if (backward.Value[1] < forward.Value[1]) {
+                inputList.AddBefore(forward, new LinkedListNode<List<int>>(new List<int>{backward.Value[0], backward.Value[1]}));
+                forward.Value[1] -= backward.Value[1];
+                backward.Value[0] = -1;
+                
+                forward = inputList.First;
+                backward = backward.Previous;
+                continue;
+            } else {
+                forward = forward.Next;
+            }
+        }
+        }
+
+        long position = 0;
+        for (var forward = inputList.First; forward != null; forward = forward.Next) {
+            for (int i = 0; i < forward.Value[1]; i++)
+            {
+                if (forward.Value[0] != -1) {
+                    result += position * forward.Value[0];
+                }
+                position++;
+            }
+        }
 
         Console.WriteLine($"Star 2: {result}");
     }
@@ -94,8 +158,8 @@ class Day9 : IAlgorithms, IStars
 
     public void Star2()
     {
-        Star2Algorithm("../../../Examples/Day9Star1Example1.txt");
+        // Star2Algorithm("../../../Examples/Day9Star1Example1.txt");
 
-        // Star2Algorithm("../../../Input/Day9Star1.txt");
+        Star2Algorithm("../../../Input/Day9Star1.txt");
     }
 }
